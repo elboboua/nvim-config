fbd82ac1c429d714a2dc3f45a5cb8439b66abaa1
Ahmad El-Bobou <ahmad.elbobou@gmail.com>
2024-09-30 08:17:46 -0500

    changes js linter to prettier

--- init.lua
+++ init.lua
@@ -713,7 +713,10 @@ require('lazy').setup({
         python = { 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        javascriptreact = { 'prettier' },
       },
     },
   },
